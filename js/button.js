c36d27c4501f54be1d7219f671788a202372ac09
vitalik <vitaliybog@yandex.ru>
2014-03-28 16:14:40 +0200

    fix

--- js/button.js
+++ js/button.js
@@ -3,4 +3,26 @@ $(document).ready(function(){
         $(this).toggleClass("active");
         $(this).siblings().toggleClass("active");
     });
+
+    $('#start-chat-closed .open-chat').click(function() {
+        $('#start-chat-closed').hide();
+        $('#start-chat-opened').show();
+    });
+
+    $('#start-chat-opened .turn-chat').click(function() {
+        $('#start-chat-opened').hide();
+        $('#start-chat-closed').show();
+    });
+
+
+    $('.login-registr .forgot-password-button').click(function() {
+       $('.login-registr').hide();
+       $('.forgot-password').show();
+    });
+
+    $('.forgot-password .actions-back').click(function() {
+       $('.forgot-password').hide();
+       $('.login-registr').show();
+    });
+
 });
