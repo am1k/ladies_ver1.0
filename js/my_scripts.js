afbc28f1cac18cb4ac7300e1e8d26127e221195a
vitalik <vitaliybog@yandex.ru>
2014-03-28 12:37:26 +0200

    fix

---
+++ js/my_scripts.js
@@ -0,0 +1,114 @@
+$(function () {
+    //script for popups
+    $('.register').click(function () {
+        $('div.'+$(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('.popup-header-registration').show();
+        $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+        return false;
+    });
+    $('.close-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        $('.popup-registration').hide();
+        return false;
+    });
+});
+
+$(function () {
+    //script for popups
+    $('.name-ladies').click(function () {
+        $('div.'+$(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('.popup-header-registration').show();
+        $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+        return false;
+    });
+    $('.close-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        $('.popup-registration').hide();
+        return false;
+    });
+});
+
+$(function () {
+    //script for popups
+    $('.planned').click(function () {
+        $('div.'+$(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('.popup-header-registration').show();
+        $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+        return false;
+    });
+    $('.close-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        $('.popup-registration').hide();
+        return false;
+    });
+});
+$(function () {
+    //script for popups
+    $('.sent-message').click(function () {
+        $('div.'+$(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('.popup-header-registration').show();
+        $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+        return false;
+    });
+    $('.close-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        $('.popup-registration').hide();
+        return false;
+    });
+});
+$(function () {
+    //script for popups
+    $('.testmonials-more').click(function () {
+        $('div.'+$(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('.popup-header-registration').show();
+        $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+        return false;
+    });
+    $('.close-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        $('.popup-registration').hide();
+        return false;
+    });
+});
+$(function () {
+    //script for popups
+    $('.popular-ladies-more').click(function () {
+        $('div.'+$(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('.popup-header-registration').show();
+        $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+        return false;
+    });
+    $('.close-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        $('.popup-registration').hide();
+        return false;
+    });
+});
+$(function () {
+    //script for popups
+    $('.nav-menu ul li').click(function () {
+        $('div.'+$(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('.popup-header-registration').show();
+        $('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+        return false;
+    });
+    $('.close-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        $('.popup-registration').hide();
+        return false;
+    });
+});
